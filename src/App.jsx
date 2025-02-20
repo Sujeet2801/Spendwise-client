86f455f2bb8bf431a9fad2404799af1776408099
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-02-20 19:49:51 +0530

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 
-import './App.css'
-import Appbar from './components/Appbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// import Appbar from './components/Appbar'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import DataProvider from './context/DataProvider'
+import Cover from './pages/Cover'
+import Response from './pages/Response'
+import UpdatePage from './pages/UpdatePage'
 
 function App() {
 
   return (
-    <div >
-      <Appbar/>
-      <Home/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <DataProvider>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Cover />} />
+          <Route path="response" element={<Response />} />
+          <Route path="about" element={<About />} />
+          <Route path="update" element={<UpdatePage />} />
+          <Route path="contact" element={<Contact/>} />
+        </Routes>
+        <Footer />
+        </DataProvider>
+    </BrowserRouter>
   )
 }
 
