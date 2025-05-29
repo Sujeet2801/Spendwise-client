531a36783ef2493e74c4cbd1fa2d205f774eb92f
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-29 11:38:54 +0530

    chore(client): Add proxy for /api/v1/users to backend server

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api/v1/users": {
+        target: "https://spendwise-website-production.up.railway.app/",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   plugins: [react()],
 })
